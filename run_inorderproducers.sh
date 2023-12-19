65facb2594795fd0a6e945e2f081bd282a931af6
Dan P <dplyukhin@gmail.com>
2023-12-19 16:54:51 -0500

    Added script to run in-order producers

---
+++ run_inorderproducers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+trap "kill 0" EXIT
+
+mvn compile
+
+mvn exec:java -Dexec.mainClass="choral.examples.futures.inorderproducers.Server" &
+sleep 1
+mvn exec:java -Dexec.mainClass="choral.examples.futures.inorderproducers.Worker2" &
+mvn exec:java -Dexec.mainClass="choral.examples.futures.inorderproducers.Worker1" &
+
+wait
