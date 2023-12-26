684cf5d6372a66f4567a2f08a70813bb351f35fe
Dan P <dplyukhin@gmail.com>
2023-12-26 16:05:31 -0500

    Switched to WrappedByteChannel, now latency is observed

--- src/main/java/choral/examples/futures/playground/Client.java
+++ src/main/java/choral/examples/futures/playground/Client.java
@@ -1,26 +1,27 @@
 package choral.examples.futures.playground;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.SocketChannel;
+
+import choral.runtime.Media.SocketByteChannel;
+import choral.runtime.WrapperByteChannel.WrapperByteChannelImpl;
 
 public class Client {
 
     public static void main(String[] args) {
-        try{
-            SocketChannel ch = SocketChannel.open();
-            ch.connect( new InetSocketAddress( Server.HOST, Server.PORT ) );
-            ch.configureBlocking( true );
+        WrapperByteChannelImpl ch =
+            new WrapperByteChannelImpl(
+                SocketByteChannel.connect(
+                    Server.HOST, Server.PORT
+                )
+            );
 
-            for (int i = 0; i < 50; i++) {
-                System.out.println("Iteration " + i + ": Client starting at time " + System.currentTimeMillis());
-                ch.write( ByteBuffer.wrap(new byte[] { 1, 1, 1, 1 }));
-                ch.read(ByteBuffer.allocate(4));
-                System.out.println("Iteration " + i + ": Client ending at time " + System.currentTimeMillis());
-                //try { Thread.sleep(100); } catch (InterruptedException e) { }
-            }
+        for (int i = 0; i < 50; i++) {
+            System.out.println("Iteration " + i + ": Client starting at time " + System.currentTimeMillis());
+            ch.com( ByteBuffer.wrap(new byte[] { 1, 1, 1, 1 }));
+            ch.com();
+            System.out.println("Iteration " + i + ": Client ending at time " + System.currentTimeMillis());
+            //try { Thread.sleep(100); } catch (InterruptedException e) { }
         }
-        catch (IOException e) {}
     }
 }
--- src/main/java/choral/examples/futures/playground/Server.java
+++ src/main/java/choral/examples/futures/playground/Server.java
@@ -1,35 +1,33 @@
 package choral.examples.futures.playground;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
 import java.nio.ByteBuffer;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+
+import choral.runtime.Media.ServerSocketByteChannel;
+import choral.runtime.WrapperByteChannel.WrapperByteChannelImpl;
 
 public class Server {
     public static final String HOST = "localhost";
     public static final int PORT = 8667;
 
     public static void main(String[] args) throws java.io.IOException {
-        try {
-            ServerSocketChannel client_listener = ServerSocketChannel.open();
-            client_listener
-                    .socket()
-                    .bind( new InetSocketAddress( Server.HOST, Server.PORT ) );
-            
-            SocketChannel ch = client_listener.accept();
-            ch.configureBlocking( true );
+        ServerSocketByteChannel client_listener =
+            ServerSocketByteChannel.at( 
+                Server.HOST, Server.PORT 
+            );
 
-            for (int i = 0; i < 50; i++) {
-                ch.read( ByteBuffer.allocate(4) );
-                System.out.println("Iteration " + i + ": Server starting at time " + System.currentTimeMillis());
-                ch.write( ByteBuffer.wrap(new byte[] { 1, 1, 1, 1 }) );
-                // try { Thread.sleep(5); } catch (InterruptedException e) { }
-                //ch.< Integer >com(1);
-                System.out.println("Iteration " + i + ": Server completed at time " + System.currentTimeMillis());
-            }
+        WrapperByteChannelImpl ch = 
+            new WrapperByteChannelImpl(
+                client_listener.getNext()
+            );
+
+        for (int i = 0; i < 50; i++) {
+            ch.com();
+            System.out.println("Iteration " + i + ": Server starting at time " + System.currentTimeMillis());
+            ch.com( ByteBuffer.wrap(new byte[] { 1, 1, 1, 1 }) );
+            // try { Thread.sleep(5); } catch (InterruptedException e) { }
+            //ch.< Integer >com(1);
+            System.out.println("Iteration " + i + ": Server completed at time " + System.currentTimeMillis());
         }
-        catch (IOException e) {}
     }
 }
