6dbd8f054e86e8fa1cb8e247e50eab44ee703263
Dan P <dplyukhin@gmail.com>
2023-12-29 12:20:57 -0500

    Easier than I thought! We just needed to send the bytes all together.

--- src/main/java/choral/examples/futures/playground/MySocketByteChannel.java
+++ src/main/java/choral/examples/futures/playground/MySocketByteChannel.java
@@ -1,6 +1,5 @@
 package choral.examples.futures.playground;
 
-import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
@@ -21,7 +20,7 @@ public class MySocketByteChannel implements SymDataChannelImpl< ByteBuffer > {
 		try {
 			SocketChannel channel = SocketChannel.open();
 			channel.connect( new InetSocketAddress( hostname, portNumber ) );
-			channel.configureBlocking( false );
+			channel.configureBlocking( true );
 			return new MySocketByteChannel( channel );
 		} catch( IOException e ) {
 			e.printStackTrace();
@@ -37,23 +36,14 @@ public class MySocketByteChannel implements SymDataChannelImpl< ByteBuffer > {
 	@Override
 	public < T extends ByteBuffer > T com() {
 		try {
-			// Read the size of the message
 			ByteBuffer buffer = ByteBuffer.allocate( 4 );
-			int read = 0;
-			while( read < 4 ) {
-				read += channel.read( buffer );
-			}
+			channel.read( buffer );
 			buffer.flip();
-			int size = buffer.getInt();
-
-			// Read the message
-			buffer = ByteBuffer.allocate( size );
-			read = 0;
-			while( read < size ) {
-				read += channel.read( buffer );
-			}
+			int transmissionLength = buffer.getInt();
+			buffer = ByteBuffer.allocate( transmissionLength );
+			channel.read( buffer );
 			buffer.flip();
-			return ( T ) buffer;
+			return (T) buffer;
 		} catch( IOException e ) {
 			throw new RuntimeException( "Could not read from channel" );
 		}
