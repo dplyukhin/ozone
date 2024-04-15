a5c9b83c1d91b9c15402f4956ee3d8edde85800d
Dan Plyukhin <dplyukhin@gmail.com>
2024-04-15 15:49:54 -0500

    Fix: servers should connect to the client, so we all agree who client1 and client2 are

--- src/main/java/choral/examples/ozone/modelserving/Client.java
+++ src/main/java/choral/examples/ozone/modelserving/Client.java
@@ -1,5 +1,6 @@
 package choral.examples.ozone.modelserving;
 
+import java.io.IOException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
@@ -9,34 +10,53 @@ import choral.channels.SymChannel_A;
 import choral.channels.SymChannel_B;
 import choral.examples.ozone.modelserving.ModelServing_Client;
 import choral.runtime.AsyncChannelImpl;
+import choral.runtime.AsyncServerSocketChannel;
 import choral.runtime.AsyncSocketChannel;
 import choral.runtime.JavaSerializer;
 
 public class Client {
 
     public static void main(String[] args) {
-        Log.debug("Connecting to server...");
-
         ScheduledExecutorService threadPool = Executors.newScheduledThreadPool(4);
-        SymChannel_A<Object> chW1 = AsyncSocketChannel.connect(
+
+        Log.debug("Connecting to other nodes...");
+
+        AsyncServerSocketChannel worker1_listener = AsyncServerSocketChannel.at( 
             new JavaSerializer(), Config.HOST, Config.WORKER1_PORT
         );
-        SymChannel_A<Object> chW2 = AsyncSocketChannel.connect(
+        AsyncServerSocketChannel worker2_listener = AsyncServerSocketChannel.at( 
             new JavaSerializer(), Config.HOST, Config.WORKER2_PORT
         );
-        AsyncChannel_A<Object> chB = new AsyncChannelImpl<>(
-            threadPool,
-            AsyncSocketChannel.connect( 
-                new JavaSerializer(), Config.HOST, Config.BATCHER_PORT
-            )
+        AsyncServerSocketChannel batcher_listener = AsyncServerSocketChannel.at( 
+            new JavaSerializer(), Config.HOST, Config.BATCHER_PORT
         );
-        Log.debug("Connection succeeded.");
 
-        ModelServing_Client prot = new ModelServing_Client(chW1, chW2, chB);
-        //for (int i = 0; i < Config.NUM_ITERATIONS; i++)
-        //    prot.on
+        try {
+            SymChannel_A<Object> chW1 = worker1_listener.getNext();
+            Log.debug("Worker 1 connected.");
+            SymChannel_A<Object> chW2 = worker2_listener.getNext();
+            Log.debug("Worker 2 connected.");
+            AsyncChannel_A<Object> chB = new AsyncChannelImpl<Object>( 
+                threadPool, batcher_listener.getNext()
+            );
+            Log.debug("Batcher connected.");
 
-        threadPool.shutdownNow();
-        Log.debug("Client done.");
+            Log.debug("Client starting!");
+
+            ModelServing_Client prot = new ModelServing_Client(chW1, chW2, chB);
+            //for (int i = 0; i < Config.NUM_ITERATIONS; i++)
+            //    prot.on
+
+        } 
+        catch (IOException e) {
+            Log.debug("Error in client, aborting: " + e.getMessage());
+        }
+        finally {
+            threadPool.shutdownNow();
+            worker1_listener.close();
+            worker2_listener.close();
+            batcher_listener.close();
+            Log.debug("Client done.");
+        }
     }
 }
