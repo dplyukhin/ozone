abe342026a83702e62dd417bfaf2ec5713d06513
Dan Plyukhin <dplyukhin@gmail.com>
2024-04-16 14:40:07 -0500

    Modified the script to run a range of benchmarks

--- scripts/concurrentmodelserving.sh
+++ scripts/concurrentmodelserving.sh
@@ -4,20 +4,32 @@ trap "kill 0" EXIT
 # mvn compile
 
 batchSize=10
-requestsPerSecond=350
-useOzone=true
-systemArgs="-DbatchSize=$batchSize -DrequestsPerSecond=$requestsPerSecond -DuseOzone=$useOzone"
 
-mvn -q -e exec:java -Dexec.mainClass="choral.examples.ozone.modelserving.Client" -Dexec.args="img.jpg" $systemArgs &
-sleep 1
-mvn -q -e exec:java -Dexec.mainClass="choral.examples.ozone.modelserving.Batcher" $systemArgs &
-sleep 1
-mvn -q -e exec:java -Dexec.mainClass="choral.examples.ozone.modelserving.Worker" -Dexec.args="--workerID 1" $systemArgs &
-sleep 1
-mvn -q -e exec:java -Dexec.mainClass="choral.examples.ozone.modelserving.Worker" -Dexec.args="--workerID 2" $systemArgs &
-sleep 1
-mvn -q -e exec:java -Dexec.mainClass="choral.examples.ozone.modelserving.Model" -Dexec.args="--modelID 1" $systemArgs &
-sleep 1
-mvn -q -e exec:java -Dexec.mainClass="choral.examples.ozone.modelserving.Model" -Dexec.args="--modelID 2" $systemArgs &
+for useOzone in false true; do
+    if [ "$useOzone" = "false" ]; then
+        requestsPerSecondValues=(125 150 175 200)
+    else
+        requestsPerSecondValues=(275 300 325 350)
+    fi
 
-wait
+    for requestsPerSecond in ${requestsPerSecondValues[@]}; do
+        systemArgs="-DbatchSize=$batchSize -DrequestsPerSecond=$requestsPerSecond -DuseOzone=$useOzone"
+
+        echo "Running with useOzone=$useOzone and requestsPerSecond=$requestsPerSecond."
+
+        mvn -q -e exec:java -Dexec.mainClass="choral.examples.ozone.modelserving.Client" -Dexec.args="img.jpg" $systemArgs &
+        sleep 1
+        mvn -q -e exec:java -Dexec.mainClass="choral.examples.ozone.modelserving.Batcher" $systemArgs &
+        sleep 1
+        mvn -q -e exec:java -Dexec.mainClass="choral.examples.ozone.modelserving.Worker" -Dexec.args="--workerID 1" $systemArgs &
+        sleep 1
+        mvn -q -e exec:java -Dexec.mainClass="choral.examples.ozone.modelserving.Worker" -Dexec.args="--workerID 2" $systemArgs &
+        sleep 1
+        mvn -q -e exec:java -Dexec.mainClass="choral.examples.ozone.modelserving.Model" -Dexec.args="--modelID 1" $systemArgs &
+        sleep 1
+        mvn -q -e exec:java -Dexec.mainClass="choral.examples.ozone.modelserving.Model" -Dexec.args="--modelID 2" $systemArgs &
+
+        wait
+    
+    done
+done
