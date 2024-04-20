c5ec8b94502064b32b397538ce5ff4f9bb091042
Dan Plyukhin <dplyukhin@gmail.com>
2024-04-19 22:35:09 -0500

    Fixed modelserving and updated readme

--- scripts/modelserving.sh
+++ scripts/modelserving.sh
@@ -6,11 +6,7 @@
 batchSize=10
 
 for useOzone in false true; do
-    if [ "$useOzone" = "false" ]; then
-        requestsPerSecondValues=(225 275 350) #(25 50 75 100 125 150 175)
-    else
-        requestsPerSecondValues=() #(25 50 75 100 125 150 175 225 275 350) # Skip rates that map to the same effective request rate 
-    fi
+    requestsPerSecondValues=(25 50 75 100 125 150 175 225 275 350) # Skip rates that map to the same effective request rate 
 
     for requestsPerSecond in ${requestsPerSecondValues[@]}; do
         systemArgs="-DbatchSize=$batchSize -DrequestsPerSecond=$requestsPerSecond -DuseOzone=$useOzone -Djava.awt.headless=true"
@@ -18,15 +14,15 @@ for useOzone in false true; do
         echo "Running with useOzone=$useOzone and requestsPerSecond=$requestsPerSecond."
 
         mvn -q -e exec:java -Dexec.mainClass="choral.examples.ozone.modelserving.Client" -Dexec.args="img.jpg" $systemArgs &
-        sleep 1
+        sleep 5
         mvn -q -e exec:java -Dexec.mainClass="choral.examples.ozone.modelserving.Batcher" $systemArgs &
-        sleep 1
+        sleep 5
         mvn -q -e exec:java -Dexec.mainClass="choral.examples.ozone.modelserving.Worker" -Dexec.args="--workerID 1" $systemArgs &
-        sleep 1
+        sleep 5
         mvn -q -e exec:java -Dexec.mainClass="choral.examples.ozone.modelserving.Worker" -Dexec.args="--workerID 2" $systemArgs &
-        sleep 1
+        sleep 5
         mvn -q -e exec:java -Dexec.mainClass="choral.examples.ozone.modelserving.Model" -Dexec.args="--modelID 1" $systemArgs &
-        sleep 1
+        sleep 5
         mvn -q -e exec:java -Dexec.mainClass="choral.examples.ozone.modelserving.Model" -Dexec.args="--modelID 2" $systemArgs &
 
         wait
